0b6a5f8e587558f7c306138f4a232f0d43f592cc
breechelugui <brendachelugui@gmail.com>
2025-09-19 23:26:05 +0300

    inital commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// index.js
 
+// Example battery array
+const batteries = [4, 5, 2, 8, 12];
+
+// Sum all batteries
+const totalBatteries = batteries.reduce((sum, val) => sum + val, 0);
+
+// Expose it globally so the test can see it
+if (typeof window !== 'undefined') {
+  window.totalBatteries = totalBatteries;
+} else {
+  global.totalBatteries = totalBatteries;
+}
 // Code your solution here
